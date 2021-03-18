6665ab1468ba68983a2b0178cc64315f233de295
Dominik <yt.dominik48n@gmail.com>
2021-03-18 07:18:32 +0100

    Create the first container and begin with the home page

---
+++ src/components/Container.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Container extends Vue {
+  @Prop(Boolean) private fluid!: boolean;
+}
+</script>
+
+<template>
+  <selection :class="['container', { fluid: fluid }]">
+    <slot />
+  </selection>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Component, Vue } from "vue-property-decorator";
+
+  import Container from "../components/Container.vue";
+
+  @Component({
+    components: {
+      Container
+    },
+  })
+
+  export default class Home extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
